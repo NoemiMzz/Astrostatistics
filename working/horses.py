343b1b3abbb84fca6aa3e209bd4fa50ce5f16402
Noemi Mezzanzanica <noemi@MacBook-Pro-di-Noemi.local>
2024-03-14 00:04:14 +0100

    Completed Prussian horses exercise

---
+++ working/horses.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import poisson
+
+################################################################################################################
+
+### plotting the death distribution ###
+x = np.arange(5)
+death = [109, 65, 22, 3, 1]
+
+counts = []   #vector with the explicit count of deaths
+for n in range(5) :
+    for i in range(death[n]) :
+        counts.append(x[n])
+
+
+### Poisson function ###
+mu = np.average(x, weights=death)   #mean as µ parameter
+distP = poisson(mu)
+
+
+### plot ###
+plt.figure()
+plt.hist(counts, bins=x, density=True, histtype="step", lw=2, color="royalblue")
+plt.plot(x, distP.pmf(x), color="red")
+plt.title("Prussian cavalryman horse-kick deaths")
+plt.xticks(np.arange(5))
+plt.xlabel("deaths")
+plt.ylabel("fraction of groups")
+plt.show()
