b5f0e2d03fd601bd988905e4d99d5de96082e952
Noemi Mezzanzanica <noemi@MacBook-Pro-di-Noemi.local>
2024-04-22 10:01:58 +0200

    Update exercises

--- working/lec10.py
+++ working/lec10.py
@@ -4,60 +4,108 @@ from tqdm import tqdm
 
 ################################################################################################################
 
-N = 10000   #number of days
+N = 100000   #number of days
+p_cc = 0.5   #probability cloudy given cloudy
+p_cs = 0.1   #probability cloudy given sunny
 p_ss = 0.9   #probability sunny given sunny
-p_sc = 0.5   #probability sunny given cloudy
+p_sc = 0.5   #probability sunny given cloudy and viceversa
 
 ################################################################################################################
 
-x = np.zeros(N)
+### sampling the weather ###
+w = [1]   #weather, starting from a sunny day
 
-sunny_fraction = np.zeros(N)
+for i in tqdm(range(1, N)):
+    if w[i-1] == 0 :
+        w.append(np.random.choice([0, 1], p=[p_cc, p_sc]))   #weather proposal given cloudy
+    elif w[i-1] == 1 :
+        w.append(np.random.choice([0, 1], p=[p_cs, p_ss]))   #weather proposal given sunny
 
-for i in tqdm(range(N)):
-    prop = np.random.uniform()   #draw a random number between 0 and 1
-    
-    if x[i-1] == 1:
-        if prop > p_ss:
-            sunny_fraction[i] = sunny_fraction[i-1] + 1
-        else:
-            sunny_fraction[i] = sunny_fraction[i-1]
-            
-    if x[i-1] == 0:
-        if prop > p_sc:
-            sunny_fraction[i] = sunny_fraction[i-1] + 1
-        else:
-            sunny_fraction[i] = sunny_fraction[i-1]
+w = np.array(w)
+sunny_fraction = np.cumsum(w) / (np.arange(N) + 1)   #fraction of sunny days
 
-################################################################################################################
+plt.figure()
+plt.plot(np.arange(N), sunny_fraction, color='crimson')
+plt.title("Trace plot")
+plt.xlabel("days")
+plt.ylabel("prob")
+plt.show()
 
-w = 0   #starting from a cloudy day
-posterior = [w]   #current weather
+plt.figure()
+plt.hist(sunny_fraction, bins=int(np.sqrt(N)), density=True, color='gold')
+plt.title("Sunny days")
+plt.xlabel("prob")
+plt.show()
 
-sunny_days = np.zeros(N)   #amount of sunny days so far
-sunny_fraction = np.zeros(N)   #cumulative fraction of sunny days
+#%%
+### sunny days ###
+burn = 25000   #burn in the first samples
+sunny_burned = sunny_fraction[burn:]
 
-for i in tqdm(range(N)):
-    w_prop = np.random.choice([0, 1])   #weather proposal   0 nad 1 need to be choosen with a probability of 0.5 and 0.9
-    acc = np.random.uniform()   #proposal probability
-    
-    
-    
-################################################################################################################
-    
-    if w_prop == 1:
-        if p_prop > p_ss:
-            sunny_days[i] = sunny_days[i-1] + 1
-            sunny_fraction[i] = sunny_days[i] / i
-        else:
-            sunny_days[i] = sunny_days[i-1]
-            sunny_fraction[i] = sunny_days[i] / i
-            
-    if w_prop == 0:
-        if p_prop > p_sc:
-            sunny_days[i] = sunny_days[i-1] + 1
-            sunny_fraction[i] = sunny_days[i] / i
-        else:
-            sunny_days[i] = sunny_days[i-1]
-            sunny_fraction[i] = sunny_days[i] / i
+# statistic with median
+med = np.median(sunny_burned)
+p1 = np.percentile(sunny_burned, 16)
+p2 = np.percentile(sunny_burned, 84)
+print("SUNNY:")
+print("median = ", np.round(med, 2))
 
+#statistic with mean
+mean = np.mean(sunny_burned)
+sigma = np.std(sunny_burned)
+print("mean = ", np.round(mean, 2))
+
+plt.figure()
+plt.hist(sunny_burned, bins=int(np.sqrt(N)), density=True, color='gold')
+plt.axvline(med, color='orangered', label="median")
+plt.axvline(p1, color='orangered', ls='dotted', label="percentiles 68%")
+plt.axvline(p2, color='orangered', ls='dotted')
+plt.title("Sunny days")
+plt.xlabel("prob")
+plt.legend()
+plt.show()
+
+plt.figure()
+plt.hist(sunny_burned, bins=int(np.sqrt(N)), density=True, color='gold')
+plt.axvline(mean, color='orangered', label="mean")
+plt.axvline(mean-sigma, color='orangered', ls='dotted', label="$\pm \sigma$")
+plt.axvline(mean+sigma, color='orangered', ls='dotted')
+plt.title("Sunny days")
+plt.xlabel("prob")
+plt.legend()
+plt.show()
+
+#%%
+### cloudy days ###
+cloudy_burned = 1 - sunny_fraction[burn:]
+
+# statistic with median
+med = np.median(cloudy_burned)
+p1 = np.percentile(cloudy_burned, 16)
+p2 = np.percentile(cloudy_burned, 84)
+print("\nCLOUDY:")
+print("median = ", np.round(med, 2))
+
+#statistic with mean
+mean = np.mean(cloudy_burned)
+sigma = np.std(cloudy_burned)
+print("mean = ", np.round(mean, 2))
+
+plt.figure()
+plt.hist(cloudy_burned, bins=int(np.sqrt(N)), density=True, color='lightskyblue')
+plt.axvline(med, color='b', label="median")
+plt.axvline(p1, color='b', ls='dotted', label="percentiles 68%")
+plt.axvline(p2, color='b', ls='dotted')
+plt.title("Cloudy days")
+plt.xlabel("prob")
+plt.legend()
+plt.show()
+
+plt.figure()
+plt.hist(cloudy_burned, bins=int(np.sqrt(N)), density=True, color='lightskyblue')
+plt.axvline(mean, color='b', label="mean")
+plt.axvline(mean-sigma, color='b', ls='dotted', label="$\pm \sigma$")
+plt.axvline(mean+sigma, color='b', ls='dotted')
+plt.title("Cloudy days")
+plt.xlabel("prob")
+plt.legend()
+plt.show()
